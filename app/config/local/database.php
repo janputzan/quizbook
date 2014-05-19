87136903516b0979c7536f848b3553f2f32d9fe0
jannie <jannie@jannie.xubuntu>
2014-05-19 19:52:04 +0100

    Updated config for db

--- app/config/local/database.php
+++ app/config/local/database.php
@@ -57,7 +57,7 @@ return array(
 			'host'      => 'localhost',
 			'database'  => 'quizbook',
 			'username'  => 'root',
-			'password'  => 's3r3n3',
+			'password'  => 'orkans',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
@@ -97,28 +97,5 @@ return array(
 	*/
 
 	'migrations' => 'migrations',
-
-	/*
-	|--------------------------------------------------------------------------
-	| Redis Databases
-	|--------------------------------------------------------------------------
-	|
-	| Redis is an open source, fast, and advanced key-value store that also
-	| provides a richer set of commands than a typical key-value systems
-	| such as APC or Memcached. Laravel makes it easy to dig right in.
-	|
-	*/
-
-	'redis' => array(
-
-		'cluster' => false,
-
-		'default' => array(
-			'host'     => '127.0.0.1',
-			'port'     => 6379,
-			'database' => 0,
-		),
-
-	),
 
 );
