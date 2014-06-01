887f989043b429d11c12138e527c0b7e475a199d
jannie <jannie@jannie.xubuntu>
2014-06-01 21:02:48 +0100

    added logic to takeQuiz method - not working yet

--- app/controllers/TakeQuizController.php
+++ app/controllers/TakeQuizController.php
@@ -8,7 +8,42 @@ class TakeQuizController extends BaseController {
 		$quiz = Quiz::whereId($id)->first();
 
 
-		return $quiz->title;
+		$questions = $quiz->question()->get();
+
+		$count = 0;
+		
+
+		$content = array();
+
+		$answers = array();
+
+		foreach ($questions as $q)
+		{
+
+			$a = $q->answer()->get();
+			$count2 = 0;
+
+			foreach($a as $answer)
+			{
+
+				$answers = array_add($answers, $count2, array('answer' => $answer->id));
+				$count2++;
+
+			}
+
+			$rightAnswer = $q->right_answer;
+
+			$question = $q->question;
+
+			$content = array_add($content, $count, array(array('answers' => $answers),
+														'rightAnswer' => $rightAnswer,
+														'question' => $question));
+			$count++;
+		}
+
+		
+
+		return $content;
 
 
 
